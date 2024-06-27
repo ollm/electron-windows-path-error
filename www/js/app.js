79dec3eeb5d57c6633ac46bd61662b44465ceafd
Oleguer Llopart <oleguer.llopart.mora@gmail.com>
2024-06-27 10:40:24 +0200

    Update files

--- www/js/app.js
+++ www/js/app.js
@@ -10,44 +10,41 @@ if(!fs.existsSync(tmpFolder))
 
 window.onload = function() {
 
+	// Copy to Tmp
 	const img = p.join(appFodler, 'www', 'image', 'image-long-filename00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000.png');
-
-	// Image 0
-	const path0 = document.querySelector('.path-0');
-	const image0 = document.querySelector('.image-0');
-	const exists0 = document.querySelector('.exists-0');
-
-	path0.innerHTML = img;
-	image0.setAttribute('src', img);
-	exists0.innerHTML = fs.existsSync(img) ? 'true' : 'false';
-
-	// Image 1
-	const path1 = document.querySelector('.path-1');
-	const image1 = document.querySelector('.image-1');
-	const exists1 = document.querySelector('.exists-1');
-
 	const longTmpFolder = p.join(tmpFolder, 'image-long-folder00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000');
+	const tmpImg = p.join(longTmpFolder, 'image-long-filename00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000.png');
 
 	if(!fs.existsSync(longTmpFolder))
 		fs.mkdirSync(longTmpFolder);
 
-	const tmpImg = p.join(longTmpFolder, 'image-long-filename00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000.png');
-
 	fs.copyFileSync(img, tmpImg);
 
-	path1.innerHTML = tmpImg;
-	image1.setAttribute('src', tmpImg);
-	exists1.innerHTML = fs.existsSync(tmpImg) ? 'true' : 'false';
+	// Paths
+	let paths = {
+		'Image in App folder': p.join(appFodler, 'www', 'image', 'image-long-filename00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000.png'),
+		'Image in App folder long': p.join(appFodler, 'www', 'image', 'folder-long-filename00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000', 'image-long-filename00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000.png'),
+		'Image in App folder long 2': p.join(appFodler, 'www', 'image', 'folder-long-filename0000000000000000000000000000000000000', 'image-long-filename00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000.png'),
+		'Image in App folder long 3': p.join(appFodler, 'www', 'image', 'folder-long-filename000000000000000000000000000000000000', 'image-long-filename00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000.png'),
+		'Image in Tmp folder': tmpImg,
+		'Image in Tmp folder short filename': p.join(tmpFolder, 'IMAGE-~1', 'IMAGE-~1.PNG'),
+	};
 
-	// Image 2
-	const path2 = document.querySelector('.path-2');
-	const image2 = document.querySelector('.image-2');
-	const exists2 = document.querySelector('.exists-2');
+	let images = '';
 
-	const shortFilenameImg = p.join(tmpFolder, 'IMAGE-~1', 'IMAGE-~1.PNG');
+	for(let key in paths)
+	{
+		const path = paths[key];
 
-	path2.innerHTML = shortFilenameImg;
-	image2.setAttribute('src', shortFilenameImg);
-	exists2.innerHTML = fs.existsSync(shortFilenameImg) ? 'true' : 'false';
+		images += key+`:
+			<br>Node.js fs.existsSync: <span>`+(fs.existsSync(path) ? 'true' : 'false')+`</span>
+			<br>Path length: <span>`+path.length+`</span>
+			<br><span>`+path+`</span>
+			<br><br>
+			<img src="`+path+`" width="30">
+			<br><br><br><br>`;
+	}
+
+	document.querySelector('.images').innerHTML = images;
 
 }
